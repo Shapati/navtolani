5478bebae25342fbc23706c98d015d66edbd1b4c
Shapati <93589375+Shapati@users.noreply.github.com>
2022-03-06 19:57:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const ham  = document.querySelector('.ham')
+const hamburger = document.querySelector('.hamburger')
+
+
+
+
+ham.addEventListener('click',()=>{
+  hamburger.classList.toggle('show')
+  
+})
